3685ffb067fcb853083870667309b1eb746f3735
ajay-kumar1 <ajay.kumar1@hp.com>
2021-04-25 10:12:27 +0000

    added code for LinearReg from scratch

---
+++ d2l/Timer.py
@@ -0,0 +1,24 @@
+import time
+
+class Timer:
+    
+    def __init__(self):
+        
+        self.times = []
+        self.start()
+        
+    def start(self):
+        self.tik = time.time()
+        
+    def stop(self):
+        self.times.append(time.time()-self.tik)
+        return self.times[-1]
+    
+    def avg(self):
+        return sum(self.times)/len(self.times)
+    
+    def sum(self):
+        return sum(self.times)
+    
+    def cusmsum(self):
+        return np.array(self.times).cumsum().tolist()
